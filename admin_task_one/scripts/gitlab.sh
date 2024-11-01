acf936c3c2c7d498770d22a195f7f3aac73fdbb8
mikhail.timofeev <mikhail.timofeev@lgepartner.com>
2024-11-01 19:22:09 +0300

    add new

--- admin_task_one/scripts/gitlab.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-
-sudo -i
-
-apt-get update
-apt-get install -y curl openssh-server ca-certificates tzdata htop
-curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
-sudo EXTERNAL_URL="http://gitlab.int" apt-get install -y gitlab-ce
--- monitoring_task/scripts/run-app.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /home/vagrant/laravel-monitoring
-export CONTAINER_PREFIX=travellist
-sudo docker compose -p $CONTAINER_PREFIX -f docker-compose.yml up -d
-
-# transfer managment rights to vagrant user (fix Permission dinied error after starting application)
-sudo chown -R vagrant:vagrant /home/vagrant/laravel-monitoring/
